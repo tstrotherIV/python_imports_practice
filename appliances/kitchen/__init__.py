cd43765eae8a0b61781b13ac16b71fe2ba29b82d
Joe Shepherd <joeshepmedia@gmail.com>
2019-09-10 10:44:30 -0500

    initial commit

---
+++ appliances/kitchen/__init__.py
@@ -0,0 +1,2 @@
+from .can_opener import CanOpener
+from .coffemaker import CoffeeMaker
---
+++ index.py
@@ -0,0 +1,13 @@
+from appliances.kitchen import Dishwasher
+from appliances.laundry import Dryer
+from appliances.laundry import Washer
+from appliances.kitchen.utility import Refrigerator
+
+whirlpool_dishwasher = Dishwasher("black")
+whirlpool_dishwasher.wash_dishes()
+
+samsung_washer = Washer("red")
+samsung_dryer = Dryer("red", "gas")
+
+lg_fridge = Refrigerator("stainless")
+lg_fridge.make_ice()
